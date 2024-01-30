fd9903a09bdc9026685e5fc8a7fe8bec3c6b1af9
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-01-30 21:52:32 +0530

    Todo App Backend Files Setup

---
+++ README.md
@@ -0,0 +1,8 @@
+## Todo app
+
+This project contains a simple TODO application
+It has the following features -
+
+- Anyone user can create a todo
+- Anyone user can see their existing todos
+- Anyone user can mark a todo as done
---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { createTodo, updateTodo } = require("./types");
+const app = express();
+app.use(express.json());
+
+app.post("/todo", (req, res) => {});
+app.get("/todos", (req, res) => {});
+app.put("/completed", (req, res) => {});
