18fe8d0b336f053432cf44e37524407f2f889968
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-01-31 18:24:50 +0530

    DB functionality updated

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.post("/todo", async (req, res) => {
   await todo.create({
     title: createPayload.title,
     description: createPayload.description,
+    completed: false,
   });
   res.json({
     mrsg: "Todo Created",
@@ -26,11 +27,23 @@ app.get("/todos", async (req, res) => {
   const todos = await todo.find({});
   res.json({ todos });
 });
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const updatePayload = req.body;
   const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
     res.status(411).json({ msg: "You sent the wrong inputs" });
     return;
   }
+
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+  res.json({
+    msg: "Todo marked as completed",
+  });
 });
